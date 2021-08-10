d0dd86500e7c7ccedaafe68eb38dbf04ab418f36
Mohamed Ahmed <programmer2011515616@gmail.com>
2021-08-10 13:05:57 +0200

    adding engines

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import config from './index.js'
 
-const CONNECTION_URL = `mongodb://${config.db.url}/${config.db.name}`
+const CONNECTION_URL = `mongodb+srv://Mhmd:cs6kwkD7aduEmSZ@cluster0.8xdx0.mongodb.net/let-us-talk?retryWrites=true&w=majority`
 
 mongoose.connect(CONNECTION_URL, {
   useNewUrlParser: true,
